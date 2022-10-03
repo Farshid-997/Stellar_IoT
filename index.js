decaa25ffa2f280822effe9ba090d0d8e47ce871
sifat18 <ibnsifat@gmail.com>
2022-10-03 12:49:00 +0600

    date updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let searchData = {};
 function get_data() {
-  var post_data = {
+  let post_data = {
     operation: "fetch_log",
     auth_user: "unive",
     auth_code: "cpkt5hsldky7r8wa835t631ohbxvrx5",
@@ -22,10 +22,9 @@ function get_data() {
 
       countStudent(result);
 
-      // datePicker(result);
-      // dummyData = result;
+      
       searchData = result;
-      // console.log(result);
+     
       multiSearch();
     },
   });
@@ -52,60 +51,29 @@ function multiSearch() {
   let userName = document.getElementById("userName").value;
   let userID = document.getElementById("userId").value;
   let startDatePicker = document.getElementById("start-date").value;
-
-  // const endDateIso = new Date(startDatePicker.value).toISOString();
-  // console.log(endDateIso);
-
+  console.log( typeof startDatePicker)
+  
+  
   let endDatePicker = document.getElementById("end-date").value;
 
-  var parseData = JSON.parse(searchData);
-  var date = "2022-09-08";
-  var date1 = "2022-09-25";
-
-  // cardContainer = document.getElementById("cards");
-  // while (cardContainer.firstChild) {
-  //   cardContainer.removeChild(cardContainer.firstChild);
-  // }
+  let parseData = JSON.parse(searchData);
+ 
+  
 
   const filterData = parseData?.log
     ?.filter((e) => e.user_name === userName)
-    // .filter((e) => e.access_date >= date && e.access_date <= date1)
+    .filter((e) => e.access_date >= startDatePicker && e.access_date <= endDatePicker)
 
     .filter((e) => e.registration_id === userID);
 
   searchResult(filterData);
   console.log("filter", filterData);
-  // filteredProperties = parseData?.log?.filter((e) => {
-  //   if (userName !== "" && e.user_name !== userName) return false;
-  //   if (startDatePicker >= e.access_date && endDatePicker <= e.access_date)
-  //     return true;
-  //   if (!isNaN(userID) && e.registration_id !== userID) return false;
-
-  //   return true;
-  // });
-
-  // filteredProperties.forEach((e) => {
-  //   var card = document.createElement("DIV");
-  //   card.classList.add("card");
-
-  //   for (var key in e) {
-  //     var propHeading = document.createElement("h2");
-  //     propHeading.innerHTML = key;
-
-  //     var propValue = document.createElement("span");
-  //     propValue.innerHTML = e[key];
-
-  //     card.appendChild(propHeading);
-  //     card.appendChild(propValue);
-  //   }
-
-  //   document.getElementById("cards").appendChild(card);
-  // });
+  
 }
 
 // Name data to show in the drop down
 function showDropDown(data) {
-  var parseData = JSON.parse(data);
+  let parseData = JSON.parse(data);
   parseData?.log?.forEach((element) => {
     let name = document.getElementById("userName");
     name.innerHTML += `<option>${element.user_name}</option>`;
@@ -113,7 +81,7 @@ function showDropDown(data) {
 }
 // id to show in the dropdown
 function showDropDownId(data) {
-  var parseData = JSON.parse(data);
+  let parseData = JSON.parse(data);
   parseData?.log?.forEach((element) => {
     let name = document.getElementById("userId");
     name.innerHTML += `<option>${element.registration_id}</option>`;
@@ -122,25 +90,25 @@ function showDropDownId(data) {
 
 //count the particular student how many days he present or absent
 function countStudent(data) {
-  var present_absent_data = document.getElementById("absent-present-data");
-  var parseData = JSON.parse(data);
+  let present_absent_data = document.getElementById("absent-present-data");
+  let parseData = JSON.parse(data);
 
   let studentCount = 0;
-  var dateObj = new Date();
+  let dateObj = new Date();
 
-  var month = dateObj.getUTCMonth() + 1;
-  var day = dateObj.getUTCDate();
-  var year = dateObj.getUTCFullYear();
+  let month = dateObj.getUTCMonth() + 1;
+  let day = dateObj.getUTCDate();
+  let year = dateObj.getUTCFullYear();
 
   newdate = year + "-" + month + "-" + day;
   let date_arr = [];
-  for (var i = 0; i < parseData?.log.length; i++) {
+  for (let i = 0; i < parseData?.log.length; i++) {
     if (parseData.log[i]?.access_date !== newdate) {
       date_arr.push({
         name: parseData.log[i]?.user_name,
         count: studentCount + 1,
       });
-      console.log(date_arr);
+      console.log("found");
     } else {
       console.log("not found");
     }
@@ -148,13 +116,13 @@ function countStudent(data) {
 
   // for loop to retreive data from array who have count of present and absent values
 
-  for (var j = 0; j < date_arr.length; j++) {
-    // var innerData = `<div>
+  for (let j = 0; j < date_arr.length; j++) {
+    // let innerData = `<div>
     // <h4> Name of the Student: ${date_arr[j]?.name} The present count:${date_arr[j]?.count}</h4>
 
     // </div>`;
 
-    var row = ` <tr>
+    let row = ` <tr>
                               
                                 <td>${date_arr[j]?.name}</td>
                                 <td>${date_arr[j]?.count}</td>
@@ -169,11 +137,11 @@ function countStudent(data) {
 // show data in a table
 
 function buildType(data) {
-  var parsingData = JSON.parse(data);
-  var table = document.getElementById("tr");
+  let parsingData = JSON.parse(data);
+  let table = document.getElementById("tr");
 
-  for (var i = 0; i < parsingData?.log.length; i++) {
-    var row = ` <tr>
+  for (let i = 0; i < parsingData?.log.length; i++) {
+    let row = ` <tr>
                                 <td>${parsingData.log[i]?.registration_id}</td>
                                 <td>${parsingData.log[i]?.department}</td>
                                 <td>${parsingData.log[i]?.access_time}</td>
@@ -190,10 +158,10 @@ function buildType(data) {
 
 // searching from table
 function searchResult(data) {
-  var table = document.getElementById("tr");
+  let table = document.getElementById("tr");
   table.innerHTML = "";
-  for (var i = 0; i < data?.length; i++) {
-    var row = ` <tr>
+  for (let i = 0; i < data?.length; i++) {
+    let row = ` <tr>
                                 <td>${data[i]?.registration_id}</td>
                                 <td>${data[i]?.department}</td>
                                 <td>${data[i]?.access_time}</td>
@@ -211,9 +179,9 @@ function searchResult(data) {
 // For pdf generator
 
 function pdfGenerator() {
-  var doc = new jsPDF();
-  var elementHTML = $("#table").html();
-  var specialElementHandlers = {
+  let doc = new jsPDF();
+  let elementHTML = $("#table").html();
+  let specialElementHandlers = {
     "#tr": function (element, renderer) {
       return true;
     },
