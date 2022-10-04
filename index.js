28fae0c9d5351fd696111264e8728cc394d1f9f2
Farshid-997 <arnob.farshid@gmail.com>
2022-10-04 13:25:35 +0600

    total count added

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function get_data() {
 
       countStudent(result);
 
-      
       searchData = result;
-     
+
       multiSearch();
     },
   });
@@ -46,29 +45,24 @@ function get_data() {
 // }
 
 function multiSearch() {
-
-
   let userName = document.getElementById("userName").value;
   let userID = document.getElementById("userId").value;
   let startDatePicker = document.getElementById("start-date").value;
-  
-  
-  
+
   let endDatePicker = document.getElementById("end-date").value;
 
   let parseData = JSON.parse(searchData);
- 
-  
 
   const filterData = parseData?.log
     ?.filter((e) => e.user_name === userName)
-    .filter((e) => e.access_date >= startDatePicker && e.access_date <= endDatePicker)
+    .filter(
+      (e) => e.access_date >= startDatePicker && e.access_date <= endDatePicker
+    )
 
     .filter((e) => e.registration_id === userID);
 
   searchResult(filterData);
   console.log("filter", filterData);
-  
 }
 
 // Name data to show in the drop down
@@ -93,7 +87,14 @@ function countStudent(data) {
   let present_absent_data = document.getElementById("absent-present-data");
   let parseData = JSON.parse(data);
 
-  let studentCount = 0;
+  let studentPresentCount = 0;
+  let studentAbsentCount = 0;
+  let totalCount = [
+    {
+      totalPresentCount: 0,
+      totalAbsentCount: 0,
+    },
+  ];
   let dateObj = new Date();
 
   let month = dateObj.getUTCMonth() + 1;
@@ -101,30 +102,46 @@ function countStudent(data) {
   let year = dateObj.getUTCFullYear();
 
   newdate = year + "-" + month + "-" + day;
-  let date_arr = [];
+
+  let date_arr = [
+    {
+      name: "name",
+      present: 0,
+      absent: 0,
+    },
+  ];
   for (let i = 0; i < parseData?.log.length; i++) {
     if (parseData.log[i]?.access_date !== newdate) {
       date_arr.push({
         name: parseData.log[i]?.user_name,
-        count: studentCount + 1,
-        
+        present: studentPresentCount + 1,
+        absent: 0,
       });
-      console.log("found");
+      totalCount.push({
+        totalPresentCount: date_arr.present + 1,
+        totalAbsentCount: date_arr.absent,
+      });
+      console.log(totalCount);
+      window.localStorage.setItem(totalCount, JSON.stringify(totalCount));
     } else {
-      console.log("not found");
+      date_arr.push({
+        name: parseData.log[i]?.user_name,
+        present: 0,
+        absent: studentAbsentCount + 1,
+      });
+
+      window.localStorage.setItem(date_arr, JSON.stringify(date_arr));
     }
   }
 
   // for loop to retreive data from array who have count of present and absent values
 
   for (let j = 0; j < date_arr.length; j++) {
-    
-
     let row = ` <tr>
                               
                                 <td>${date_arr[j]?.name}</td>
-                                <td>${date_arr[j]?.count}</td>
-                                <td>${0}</td>
+                                <td>${date_arr[j]?.present}</td>
+                                <td>${date_arr[j]?.absent}</td>
                                 </tr>
 
                           `;
@@ -176,19 +193,62 @@ function searchResult(data) {
 
 // For pdf generator
 
+// function pdfGenerator() {
+//   let doc = new jsPDF();
+//   let elementHTML = $("#table").html();
+//   let specialElementHandlers = {
+//     "#tr": function (element, renderer) {
+//       return true;
+//     },
+//   };
+//   doc.fromHTML(elementHTML, 15, 15, {
+//     width: 170,
+//     elementHandlers: specialElementHandlers,
+//   });
+
+//   doc.save("sample-document.pdf");
+// }
+
 function pdfGenerator() {
-  let doc = new jsPDF();
-  let elementHTML = $("#table").html();
-  let specialElementHandlers = {
-    "#tr": function (element, renderer) {
+  var pdf = new jsPDF("p", "pt", "letter");
+  // source can be HTML-formatted string, or a reference
+  // to an actual DOM element from which the text will be scraped.
+  source = $("#containers")[0];
+
+  // we support special element handlers. Register them with jQuery-style
+  // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+  // There is no support for any other type of selectors
+  // (class, of compound) at this time.
+  specialElementHandlers = {
+    // element with id of "bypass" - jQuery style selector
+    "#bypassme": function (element, renderer) {
+      // true = "handled elsewhere, bypass text extraction"
       return true;
     },
   };
-  doc.fromHTML(elementHTML, 15, 15, {
-    width: 170,
-    elementHandlers: specialElementHandlers,
-  });
+  margins = {
+    top: 80,
+    bottom: 60,
+    left: 40,
+    width: 522,
+  };
+  // all coords and widths are in jsPDF instance's declared units
+  // 'inches' in this case
+  pdf.fromHTML(
+    source, // HTML string or DOM elem ref.
+    margins.left, // x coord
+    margins.top,
 
-  // Save the PDF
-  doc.save("sample-document.pdf");
+    {
+      // y coord
+      width: margins.width, // max width of content on PDF
+      elementHandlers: specialElementHandlers,
+    },
+    function (dispose) {
+      // dispose: object with X, Y of the last line add to the PDF
+      //          this allow the insertion of new lines after html
+      pdf.save("studentData.pdf");
+    },
+    margins
+  );
 }
